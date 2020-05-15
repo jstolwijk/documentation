3c8219ba90198bc0cc6644057400fa2875506f5b
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-05-15 23:06:59 +0200

    Setup tailwind

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+// https://github.com/tailwindcss/setup-examples/tree/master/examples/nextjs
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+module.exports = {
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
+};
